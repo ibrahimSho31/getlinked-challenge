88fca108c0faf18a9870c14173c00691706c2e31
Ibrahim <shokoyaib@gmail.com>
2023-09-23 04:28:04 +0100

    update

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import { BarLoader } from 'react-spinners';
-import { useState, useEffect } from "react";
-import { Man, Globe, Woman, thinkBig, people, Purp2, Sitting } from './assets';
 const loadingSpinnerStyle = css`
   margin-top: 20%;
   
@@ -11,118 +9,7 @@ const loadingSpinnerStyle = css`
 
 
 const LoadingSpinner = () => {
-  const [imageLoaded, setImageLoaded] = useState(false);
 
-
-useEffect(() => {
-  const image = new Image();
-  image.src = Man;
-
-  image.onload = () => {
-    // Set the state to indicate that the image has loaded
-    setImageLoaded(true);
-  };
-
-  // Clean up the event listener when the component unmounts
-  return () => {
-    image.onload = null;
-  };
-}, []);
-
-
-useEffect(() => {
-  const image = new Image();
-  image.src = Globe;
-
-  image.onload = () => {
-    // Set the state to indicate that the image has loaded
-    setImageLoaded(true);
-  };
-
-  // Clean up the event listener when the component unmounts
-  return () => {
-    image.onload = null;
-  };
-}, []);
-
-
-useEffect(() => {
-  const image = new Image();
-  image.src = Woman;
-
-  image.onload = () => {
-    // Set the state to indicate that the image has loaded
-    setImageLoaded(true);
-  };
-
-  // Clean up the event listener when the component unmounts
-  return () => {
-    image.onload = null;
-  };
-}, []);
-
-
-useEffect(() => {
-  const image = new Image();
-  image.src = people;
-
-  image.onload = () => {
-    // Set the state to indicate that the image has loaded
-    setImageLoaded(true);
-  };
-
-  // Clean up the event listener when the component unmounts
-  return () => {
-    image.onload = null;
-  };
-}, []);
-
-
-useEffect(() => {
-  const image = new Image();
-  image.src = thinkBig;
-
-  image.onload = () => {
-    // Set the state to indicate that the image has loaded
-    setImageLoaded(true);
-  };
-
-  // Clean up the event listener when the component unmounts
-  return () => {
-    image.onload = null;
-  };
-}, []);
-
-
-useEffect(() => {
-  const image = new Image();
-  image.src = Purp2;
-
-  image.onload = () => {
-    // Set the state to indicate that the image has loaded
-    setImageLoaded(true);
-  };
-
-  // Clean up the event listener when the component unmounts
-  return () => {
-    image.onload = null;
-  };
-}, []);
-
-useEffect(() => {
-  const image = new Image();
-  image.src = Sitting;
-
-  image.onload = () => {
-    // Set the state to indicate that the image has loaded
-    setImageLoaded(true);
-  };
-
-  // Clean up the event listener when the component unmounts
-  return () => {
-    image.onload = null;
-  };
-}, []);
 
   return (
     <div className="loader h-[100vh] flex flex-col items-center justify-center">
